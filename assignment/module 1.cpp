7fcfbd2be33fec48726ba1ac6d91524a85f1db0e
devanshumaheta <113577434+devanshumaheta@users.noreply.github.com>
2022-10-12 11:08:55 +0530

    Add files via upload

---
+++ assignment/module 1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    printf("Name= Devanshu Maheta");
+    printf("\nBirthdate=01 september 2004 ");
+    printf("\nAge=18");
+    printf("\nAddress= sant krupa jamnagr main road");
+}

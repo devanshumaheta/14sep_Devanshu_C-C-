e922936871a4d165f6fa4cb526769aa2a99f1194
devanshumaheta <113577434+devanshumaheta@users.noreply.github.com>
2022-10-12 11:17:47 +0530

    Add files via upload

---
+++ task/if else condition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter the value :");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        printf("a no is maximum.%d",a);
+    
+    }
+    else
+    {
+        printf("a no is minimun.%d",a);
+        
+    }
+}

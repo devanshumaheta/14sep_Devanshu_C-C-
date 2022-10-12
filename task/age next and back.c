4ddc116681f6561d73bf770cb4c320ed16cb477a
devanshumaheta <113577434+devanshumaheta@users.noreply.github.com>
2022-10-12 11:17:21 +0530

    Add files via upload

---
+++ task/age next and back.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int currentage;
+    printf("my current age is :");
+    scanf("%d",&currentage);
+    currentage++;
+    printf("my next year age is :%d",currentage);
+    currentage--;
+    currentage--;
+    printf("\nmy previous year age is :%d",currentage);
+}
